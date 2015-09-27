d8755ef32a7260c7a77e552adf617d096099bc48
gernots <gernot.starke@innoq.com>
2015-09-27 16:56:15 +0200

    created project - prepared for menubar application

---
+++ hiddenFilesToggler/hiddenFilesToggler/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  hiddenFilesToggler
+//
+//  Created by Dr. Gernot Starke on 27.09.15.
+//  Copyright © 2015 Dr. Gernot Starke. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
