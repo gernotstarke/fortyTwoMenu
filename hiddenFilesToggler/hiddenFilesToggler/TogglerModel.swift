6c813dc1269493300bcd4e0459ba98d76f518723
gernots <gernot.starke@innoq.com>
2015-09-29 08:21:27 +0200

    updated icons

---
+++ hiddenFilesToggler/hiddenFilesToggler/TogglerModel.swift
@@ -0,0 +1,9 @@
+//
+//  TogglerModel.swift
+//  hiddenFilesToggler
+//
+//  Created by Dr. Gernot Starke on 28.09.15.
+//  Copyright © 2015 Dr. Gernot Starke. All rights reserved.
+//
+
+import Foundation
