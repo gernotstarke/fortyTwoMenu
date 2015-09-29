06873f2a1b6cd1fb45b0992538ea887c771506f2
gernots <gernot.starke@innoq.com>
2015-09-29 08:21:50 +0200

    removed older icon,

--- hiddenFilesToggler/hiddenFilesToggler/AppDelegate.swift
+++ hiddenFilesToggler/hiddenFilesToggler/AppDelegate.swift
@@ -17,25 +17,52 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var statusMenu: NSMenu!
 
    
+    var togglerModel = TogglerModel()
+    
     // get reference to statusBar (aka menuBar)
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
 
     
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // statusIcon is the name of the imageSet in the Assets.xcassets folder
-        let icon = NSImage(named: "statusIcon" )
+        let icon = NSImage(named: "offStatusIcon" )
         icon?.template = false
-        //icon.setTemplate(true)
         
         statusItem.image = icon
         
         statusItem.menu = statusMenu
+    }
+    
+    // quit the application
+    @IBAction func quitAction(sender: NSMenuItem) {
+        
     }
 
     @IBAction func menuClicked(sender: NSMenuItem) {
         print("toggle hidden files clicked...")
         let task = NSTask()
         task.launchPath = "/usr/bin/defaults"
+        
+        if (sender.state == NSOnState) {
+            sender.state = NSOffState
+            
+            // task expects array
+            task.arguments = ["write", "com.apple.finder", "AppleShowAllFiles", "NO"]
+        }
+        else {
+            sender.state = NSOnState
+            
+            // task expects array
+            task.arguments = ["write", "com.apple.finder", "AppleShowAllFiles", "YES"]
+        }
+        
+        task.launch()
+        task.waitUntilExit()
+        
+        let killFinderTask = NSTask()
+        killFinderTask.launchPath = "usr/bin/killall"
+        killFinderTask.arguments = ["Finder"]
+        killFinderTask.launch()
     }
     
 }
--- hiddenFilesToggler/hiddenFilesToggler/TogglerModel.swift
+++ hiddenFilesToggler/hiddenFilesToggler/TogglerModel.swift
@@ -7,3 +7,51 @@
 //
 
 import Foundation
+
+class TogglerModel {
+    
+//    enum BoolToState {
+//        case true {
+//            state: "On", boolName: "Yes"}
+//        case false: {state: "Off", boolName: "No"}
+//    }
+    
+    init() {
+        showAllFilesState = false
+    }
+    
+    var showAllFilesState: Bool
+    
+    let menuItemPrefix = "Toggle hidden files (currently:"
+    var menuItemText: String {
+        get {
+           return "\(menuItemPrefix) \(translateBoolToState(showAllFilesState)))"
+        }
+    }
+    
+    
+    // uses the default-binary to determine the current state
+    // as it takes rather long, we try to do it only once,
+    // and later on manage this state ourself.
+    func determineInitialShowAllFilesState() -> Bool {
+        return false
+    }
+    
+    func setShowAllFilesState(toState: Bool) {
+        
+    }
+    
+    func setMenuItemText() {
+        
+    }
+    
+    func translateBoolToState(bool: Bool) -> String {
+        if bool {
+            return  "On"
+        }
+        else {
+            return "Off"
+        }
+    }
+    
+} // TogglerModel
