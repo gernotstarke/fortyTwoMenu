6071a013f0703e41e4280f2d90044a29574ce461
gernots <gernot.starke@innoq.com>
2015-10-07 10:11:29 +0200

    updated Model

--- hiddenFilesToggler/hiddenFilesToggler/TogglerModel.swift
+++ hiddenFilesToggler/hiddenFilesToggler/TogglerModel.swift
@@ -10,32 +10,75 @@ import Foundation
 
 class TogglerModel {
     
-//    enum BoolToState {
-//        case true {
-//            state: "On", boolName: "Yes"}
-//        case false: {state: "Off", boolName: "No"}
-//    }
     
-    init() {
-        showAllFilesState = false
-    }
+    var showAllFilesState = false
     
-    var showAllFilesState: Bool
+    let statusOffIconName = "offStatusIcon"
+    let statusOnIconName  = "onStatusIcon"
+    
     
     let menuItemPrefix = "Toggle hidden files (currently:"
     var menuItemText: String {
         get {
-           return "\(menuItemPrefix) \(translateBoolToState(showAllFilesState)))"
+            return "\(menuItemPrefix) \(translateBoolToState(showAllFilesState)))"
+        }
+    }
+   
+    // define an operating system task from which we can call
+    // the "default" application
+    private let defaultTask = NSTask()
+    
+    
+    init() {
+        // initialize
+        defaultTask.launchPath = "/usr/bin/defaults"
+        
+        showAllFilesState = self.determineInitialShowAllFilesState()
+    }
+    
+    
+    //
+    func currentIconName( currentState: Bool) -> String {
+        switch currentState {
+            case true: return statusOnIconName
+            case false: return statusOffIconName
         }
     }
     
-    
-    // uses the default-binary to determine the current state
+    // uses the "default" application to determine the current state
     // as it takes rather long, we try to do it only once,
     // and later on manage this state ourself.
     func determineInitialShowAllFilesState() -> Bool {
-        return false
+        // defaultTask expects array
+        defaultTask.arguments = ["read", "com.apple.finder", "AppleShowAllFiles"]
+ 
+        let pipe = NSPipe()
+        defaultTask.standardOutput = pipe
+        
+        defaultTask.launch()
+        defaultTask.waitUntilExit()
+
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let output: NSString = NSString(data: data, encoding: NSUTF8StringEncoding)!
+        
+        let result: Bool
+        
+        //print(output)
+        
+        switch output {
+        case "No":
+            result = false
+            
+        case "Yes":
+            result = true
+            
+        default:
+            result = false
+        }
+        
+        return result
     }
+    
     
     func setShowAllFilesState(toState: Bool) {
         
