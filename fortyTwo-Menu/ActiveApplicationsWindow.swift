9a5630f9d51240b95148111a1e555955618731ef
gernots <gernot.starke@innoq.com>
2015-10-13 11:29:54 +0200

    added ActiveApplicationsWindows

---
+++ fortyTwo-Menu/ActiveApplicationsWindow.swift
@@ -0,0 +1,19 @@
+//
+//  ActiveApplicationsWindow.swift
+//  fortyTwo-Menu
+//
+//  Created by Dr. Gernot Starke on 12.10.15.
+//  Copyright © 2015 Dr. Gernot Starke. All rights reserved.
+//
+
+import Cocoa
+
+class ActiveApplicationsWindow: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
